4e398a798e83dac9b8e221447c5dd1b216af763f
Zekins <zekins3366@gmail.com>
2025-03-11 18:55:27 +0300

    zekinsfix1

--- Content.Server/_Wega/GameTicking/Rules/VeilCultRuleSystem.cs
+++ Content.Server/_Wega/GameTicking/Rules/VeilCultRuleSystem.cs
@@ -1,45 +1,22 @@
 using System.Linq;
-using Content.Server.Actions;
-using Content.Server.Administration.Logs;
 using Content.Server.Antag;
-using Content.Server.Veil.Cult;
-using Content.Server.Body.Components;
-using Content.Server.Body.Systems;
 using Content.Server.GameTicking.Rules.Components;
-using Content.Server.Mind;
-using Content.Server.Roles;
 using Content.Server.RoundEnd;
 using Content.Shared.Veil.Cult;
 using Content.Shared.Veil.Cult.Components;
-using Content.Shared.Body.Components;
 using Content.Shared.Clumsy;
 using Content.Shared.CombatMode.Pacification;
-using Content.Shared.Database;
 using Content.Shared.GameTicking.Components;
-using Content.Shared.Hands.EntitySystems;
-using Content.Shared.Humanoid;
 using Content.Shared.Mobs;
 using Content.Shared.NPC.Prototypes;
-using Content.Shared.NPC.Systems;
 using Content.Shared.Zombies;
-using Robust.Shared.Audio;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Timing;
 
 namespace Content.Server.GameTicking.Rules
 {
     public sealed class VeilCultRuleSystem : GameRuleSystem<VeilCultRuleComponent>
     {
-        [Dependency] private readonly ActionsSystem _action = default!;
         [Dependency] private readonly AntagSelectionSystem _antag = default!;
-        [Dependency] private readonly BodySystem _body = default!;
-        [Dependency] private readonly VeilCultSystem _cult = default!;
-        [Dependency] private readonly IEntityManager _entityManager = default!;
-        [Dependency] private readonly IAdminLogManager _adminLogManager = default!;
-        [Dependency] private readonly MindSystem _mind = default!;
-        [Dependency] private readonly NpcFactionSystem _npcFaction = default!;
-        [Dependency] private readonly RoleSystem _role = default!;
-        [Dependency] private readonly SharedHandsSystem _hands = default!;
         [Dependency] private readonly RoundEndSystem _roundEndSystem = default!;
 
         public readonly ProtoId<NpcFactionPrototype> VeilCultNpcFaction = "VeilCult";
@@ -48,20 +25,25 @@ namespace Content.Server.GameTicking.Rules
         {
             base.Initialize();
 
-            SubscribeLocalEvent<VeilCultRuleComponent, ComponentStartup>(OnRuleStartup);
             SubscribeLocalEvent<VeilCultRuleComponent, AfterAntagEntitySelectedEvent>(OnCultistSelected);
 
             SubscribeLocalEvent<GodCalledEvent>(OnGodCalled);
             SubscribeLocalEvent<RitualConductedEvent>(OnRitualConducted);
 
-            SubscribeLocalEvent<AutoCultistComponent, ComponentStartup>(OnAutoCultistAdded);
             SubscribeLocalEvent<VeilCultistComponent, ComponentRemove>(OnComponentRemove);
             SubscribeLocalEvent<VeilCultistComponent, MobStateChangedEvent>(OnMobStateChanged);
             SubscribeLocalEvent<VeilCultistComponent, EntityZombifiedEvent>(OnOperativeZombified);
         }
 
+        private void OnCultistSelected(Entity<VeilCultRuleComponent> mindId, ref AfterAntagEntitySelectedEvent args)
+        {
+            var ent = args.EntityUid;
+            MakeCultist(ent);
+        }
+
         private void MakeCultist(EntityUid ent)
         {
+            /*
             var actionPrototypes = new[]
             {
                 VeilCultistComponent.CultObjective,
@@ -73,6 +55,7 @@ namespace Content.Server.GameTicking.Rules
             {
                 _action.AddAction(ent, actionPrototype);
             }
+            */
 
             var componentsToRemove = new[]
             {
@@ -85,28 +68,6 @@ namespace Content.Server.GameTicking.Rules
                 if (HasComp(ent, compType))
                     RemComp(ent, compType);
             }
-
-        }
-
-        private void OnAutoCultistAdded(EntityUid uid, AutoCultistComponent comp, ComponentStartup args)
-        {
-            if (!_mind.TryGetMind(uid, out var mindId, out var mind) || HasComp<VeilCultistComponent>(uid))
-            {
-                RemComp<AutoCultistComponent>(uid);
-                return;
-            }
-
-            _npcFaction.AddFaction(uid, VeilCultNpcFaction);
-            var culsistComp = EnsureComp<VeilCultistComponent>(uid);
-            _adminLogManager.Add(LogType.Mind, LogImpact.Medium, $"{ToPrettyString(uid)} converted into a Veil Cult");
-            if (mindId == default || !_role.MindHasRole<VeilCultistComponent>(mindId))
-                _role.MindAddRole(mindId, "MindRoleVeilCultist");
-            if (mind?.Session != null)
-                _antag.SendBriefing(mind.Session, MakeBriefing(uid), Color.Red, new SoundPathSpecifier("/Audio/_Wega/Ambience/Antag/veilcult_start.ogg"));
-            RemComp<AutoCultistComponent>(uid);
-
-            MakeCultist(uid);
-            var query = QueryActiveRules();
 
         }
 
