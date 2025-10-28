264f7b7d0cf2c51bf76c43be6cb9960618a4f479
liegend <2008.morozov.ri@gmail.com>
2025-10-28 10:10:55 +0700

    бебебе

--- Content.Shared/Roles/JobPrototype.cs
+++ Content.Shared/Roles/JobPrototype.cs
@@ -5,7 +5,7 @@ using Content.Shared.Players.PlayTimeTracking;
 using Content.Shared.StatusIcon;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
-using Content.Server.Roles.Jobs;
+using Content.Client.Mind;
 
 namespace Content.Shared.Roles
 {
