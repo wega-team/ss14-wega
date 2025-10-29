25c367691fb8ad61aee8f3f961fd65a60f6eb88e
liegend <2008.morozov.ri@gmail.com>
2025-10-29 09:20:04 +0700

    Update StationSpawningSystem.cs

--- Content.Server/Station/Systems/StationSpawningSystem.cs
+++ Content.Server/Station/Systems/StationSpawningSystem.cs
@@ -87,9 +87,9 @@ public sealed class StationSpawningSystem : SharedStationSpawningSystem
         EntityCoordinates coordinates,
         ProtoId<JobPrototype>? job,
         HumanoidCharacterProfile? profile,
-        ProtoId<RoleTypePrototype>? roleType,
         EntityUid? station,
-        EntityUid? entity = null
+        EntityUid? entity = null,
+        ProtoId<RoleTypePrototype>? roleType = null
         )
     {
         _prototypeManager.Resolve(job, out var prototype);
