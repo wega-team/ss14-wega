5ee531f2ea51e87dc44217ec6c9d8a3df3d7ef81
Laserkaier <you@example.com>
2026-03-05 22:54:34 +0300

    Опять обновления

--- Content.Server/_Wega/Borer/BaseBorerSystem.cs
+++ Content.Server/_Wega/Borer/BaseBorerSystem.cs
@@ -5,6 +5,8 @@ using Robust.Server.Containers;
 using Content.Shared.Actions;
 using Content.Shared.Actions.Components;
 using Content.Server.Popups;
+using Content.Shared.Borer.WormEvent;
+using Content.Shared.IdentityManagement.Components;
 
 namespace Content.Server.Borer.BorerInfectedSystem;
 
@@ -12,16 +14,50 @@ public sealed class BaseBorerSystem : EntitySystem
 {
     [Dependency] private readonly SharedActionsSystem _actions = default!;
     [Dependency] private readonly PopupSystem _popupSystem = default!;
+    [Dependency] private readonly SharedContainerSystem _containerSystem = default!;
+    [Dependency] private readonly SharedDoAfterSystem _doAfterSystem = default!;
+
 
     public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<BorerComponent, InfectionBorerEvent>(OnInfectionUse);
+        SubscribeLocalEvent<BorerComponent, InfectEvent>(Infection)
     }
 
     private void OnInfectionUse(Entity<BorerComponent> ent, ref ComponentInit args)
     {
-         _popupSystem.
-        if 
+        if (!HasComp<IdentityComponent>(args.Target))
+            return;
+        if (HasComp<SSDIndicator>(args.Target) && (args.Target.SSDIndicator.InSSD == True))
+            return;
+        if (args.Handled || _whitelistSystem.IsWhitelistFailOrNull(ent.Comp.Whitelist, args.Target))
+            return;
+
+        var target = args.Target
+
+        if (ent.Comp.Owner is not null)
+        {
+            _popup.PopupEntity(Loc.GetString("Владелец уже есть"), uid, uid, PopupType.Medium);
+            return;
+        }
+        if (HasComp<WormInfectedComponent>(target))
+        {
+            _popup.PopupEntity(Loc.GetString("Он уже заражён"), uid, uid, PopupType.Medium);
+            return;
+        }
+        
+        var doAfterEventsArgs = new DoAfterArgs(EntityManager, ent.Owner, Entity.Comp.TimeToInfect, new InfectEvent(), Entity.Owner, target: target , used: Entity.Owner)
+        {
+            BreakOnDamage = true,
+            BreakOnMove = true,
+            NeedHand = false,
+        };
+
+        if (!_doAfter.TryStartDoAfter(args))
+            return;
+    }
+    private void InfectEvent(Entity<BorerComponent> end, ref InfectEvent args)
+    {
     }
 }
--- Content.Shared/_Wega/Borer/WormEvents.cs
+++ Content.Shared/_Wega/Borer/WormEvents.cs
@@ -1,5 +1,13 @@
 using Content.Shared.Actions;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Borer.WormEvent;
 
 public sealed partial class InfectionBorerEvent : EntityTargetActionEvent
+{
+}
+
+[Serializable, NetSerializable]
+public sealed partial class ImplantEvent : SimpleDoAfterEvent
 {
 }
