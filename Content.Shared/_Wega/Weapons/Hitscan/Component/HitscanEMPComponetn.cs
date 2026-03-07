b7bc500cf4457a267f9ffbf633c98741e5d192cc
GaMEWAR360 <Gamewar816@gmail.com>
2026-03-07 07:28:34 +0300

    Mining_borg

---
+++ Content.Shared/_Wega/Weapons/Hitscan/Component/HitscanEMPComponetn.cs
@@ -0,0 +1,28 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared.Weapons.Hitscan.Components;
+
+/// <summary>
+/// Allows for hitscan entities to ignite their targets. This component modifies the ignition chance, as well as how many stacks are added once ignited.
+/// </summary>
+[RegisterComponent, AutoGenerateComponentState(true)]
+public sealed partial class HitscanEMPComponent : Component
+{
+    /// <summary>
+    /// EMP range.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public float Range = 1.0f;
+
+    /// <summary>
+    /// How much energy (in Joules) will be consumed per battery in range.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public float EnergyConsumption;
+
+    /// <summary>
+    /// How long it disables targets.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public TimeSpan DisableDuration = TimeSpan.FromSeconds(10);
+}
