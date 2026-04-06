3337e6b72f692263f7853d680e17e3c48ba8ddb6
MartynDew <aminobust3@gmail.com>
2026-04-06 22:50:20 +0300

    корвах-адд
    Co-authored-by: Zekins <136648667+Zekins3366@users.noreply.github.com>

--- Content.Shared/Weapons/Ranged/Components/BallisticAmmoProviderComponent.cs
+++ Content.Shared/Weapons/Ranged/Components/BallisticAmmoProviderComponent.cs
@@ -58,10 +58,11 @@ public sealed partial class BallisticAmmoProviderComponent : Component
     [DataField]
     public TimeSpan FillDelay = TimeSpan.FromSeconds(0.5);
 
-
+    // Corvax-Wega-Add-start
     /// <summary>
     /// TryStartDoAfter boolean for BreakOnMove, Corvax Wega thing
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite), DataField]
     public bool BreakOnMoveFill = true;
+    // Corvax-Wega-Add-start
 }
