31c7204186733f27e25ec59d08519376118908cf
liegend <2008.morozov.ri@gmail.com>
2026-05-10 13:35:26 +0700

    мое)

--- Content.Server/_Wega/StationEvents/Events/RadiationOutburstRule.cs
+++ Content.Server/_Wega/StationEvents/Events/RadiationOutburstRule.cs
@@ -14,7 +14,7 @@ using Content.Shared.Ghost;
 
 namespace Content.Server.StationEvents.Events;
 
-public sealed class RadiationOutburstRule : StationEventSystem<RadiationOutburstRuleComponent>
+public sealed class RadiationOutburstRule : StationEventSystem<RadiationOutburstRuleComponent> //port only with codeowner permision @4_ydo
 {
     [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly SharedContainerSystem _containerSystem = default!;
