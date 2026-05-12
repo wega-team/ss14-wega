8df7abfb2bbcb1de4ae0ad8b08a24729999fd5d4
liegend <2008.morozov.ri@gmail.com>
2026-05-12 18:39:52 +0700

    да пошла нахуй и так все работает

--- Content.Client/_Wega/Shader/ThermalVisionSystem.cs
+++ Content.Client/_Wega/Shader/ThermalVisionSystem.cs
@@ -2,10 +2,7 @@ using Content.Shared.Inventory;
 using Content.Shared.Thermal.Components;
 using Content.Shared.Hands;
 using Content.Shared.Inventory.Events;
-using Content.Shared.Trigger.Components.Triggers;
-using Robust.Shared.Timing;
 using Robust.Shared.GameObjects;
-using Robust.Client.Player;
 using Robust.Client.Graphics; //port only with permission of the code creator
 
 namespace Content.Client.Thermal.EntitySystems;
@@ -13,7 +10,6 @@ namespace Content.Client.Thermal.EntitySystems;
 public sealed class ThermalVisionFovSystem : EntitySystem
 {
     [Dependency] private readonly IEyeManager _eyeManager = default!;
-    [Dependency] private readonly IPlayerManager _playerManager = default!;
 
     private bool _originalDrawFov = true;
 
@@ -27,18 +23,12 @@ public sealed class ThermalVisionFovSystem : EntitySystem
 
     private void OnThermalEquipped(EntityUid uid, ThermalVisionComponent component, GotEquippedEvent args)
     {
-        if (args.Equipee != _playerManager.LocalEntity)
-            return;
-
         _originalDrawFov = _eyeManager.CurrentEye.DrawFov;
         _eyeManager.CurrentEye.DrawFov = false;
     }
 
     private void OnThermalUnequipped(EntityUid uid, ThermalVisionComponent component, GotUnequippedEvent args)
     {
-        if (args.Equipee != _playerManager.LocalEntity)
-            return;
-
         _eyeManager.CurrentEye.DrawFov = _originalDrawFov;
     }
 }
