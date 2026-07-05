1484af71f5ae6989c2310fb1f22b4c575c01c852
Kirill2253 <96187155+Kirill2253@users.noreply.github.com>
2026-07-05 16:00:07 +0300

    Термалки (#461)
    * thermal
    
    * Update uplink_catalog.yml
    
    * yeah
    
    * Update InventorySystem.Relay.cs
    
    * Update SharedToggleableEquipmentHudSystem.cs
    
    ---------
    
    Co-authored-by: Kirill2253 <kirillsuhin5@gmail.con>
    Co-authored-by: MartynDew <aminobust3@gmail.com>

---
+++ Content.Client/_Wega/Overlays/ThermalVisionOverlaySystem.cs
@@ -0,0 +1,44 @@
+using Content.Shared.Inventory.Events;
+using Content.Shared.Overlays;
+using Content.Shared.Shaders;
+using Robust.Client.Graphics;
+using Robust.Shared.GameStates;
+
+namespace Content.Client.Overlays;
+
+/// <summary>
+/// Enables the <see cref="ThermalVisionOverlay"/> whenever the local player has a
+/// <see cref="ThermalVisionComponent"/>, either directly or on a worn item.
+/// </summary>
+public sealed partial class ThermalVisionSystem : ToggleableEquipmentHudSystem<ThermalVisionComponent>
+{
+    [Dependency] private IOverlayManager _overlayMan = default!;
+
+    private ThermalVisionOverlay _overlay = default!;
+
+    public override void Initialize()
+    {
+		base.Initialize();
+		SubscribeLocalEvent<ThermalVisionComponent, AfterAutoHandleStateEvent>(OnHandleState);
+        _overlay = new ThermalVisionOverlay(EntityManager);
+    }
+
+	public void OnHandleState(Entity<ThermalVisionComponent> ent, ref AfterAutoHandleStateEvent args)
+    {
+        RefreshOverlay();
+    }
+
+    protected override void UpdateInternal(RefreshEquipmentHudEvent<ThermalVisionComponent> component)
+    {
+        base.UpdateInternal(component);
+
+        _overlayMan.AddOverlay(_overlay);
+    }
+
+    protected override void DeactivateInternal()
+    {
+        base.DeactivateInternal();
+
+        _overlayMan.RemoveOverlay(_overlay);
+    }
+}
