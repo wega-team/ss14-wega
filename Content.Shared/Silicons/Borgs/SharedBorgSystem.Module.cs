0b17b20997cc1552b8750ee7a58ca749442f36fe
GaMEWAR360 <Gamewar816@gmail.com>
2026-09-11 01:49:01 +0300

    fix

--- Content.Shared/Silicons/Borgs/SharedBorgSystem.Module.cs
+++ Content.Shared/Silicons/Borgs/SharedBorgSystem.Module.cs
@@ -120,8 +120,13 @@ public abstract partial class SharedBorgSystem
                 var handId = $"{GetNetEntity(module.Owner)}-hand-{i}";
                 if (itemModuleComp.StoredItems.TryGetValue(handId, out var item))
                 {
-                    _container.Remove(item, container, destination: coordinates);
-                    itemModuleComp.StoredItems.Remove(handId);
+					// Corvax-Wega-Start
+					if (!_tagSystem.HasTag(item, ItemborgTag))
+					{
+						_container.Remove(item, container, destination: coordinates);
+						itemModuleComp.StoredItems.Remove(handId);
+					}
+					// Corvax-Wega-End
                 }
             }
         }
